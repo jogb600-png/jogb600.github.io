2d8b5b1d2300086ad3b7597de0026d334755233b
jman1593 <josh@pettits.net>
2025-12-03 17:18:25 -0700

    e

--- cards-data-test.js
+++ cards-data-test.js
@@ -4,4 +4,5 @@ export default [
 { href: './html/Hollow knight degraded/index.html', imgSrc: 'hollow knight degraded.jpeg', name: 'Hollow knight degraded' , 'page': 5, 'id': 874 },
 { href: './html/Fabled Conquerors/index.html', imgSrc: 'fabled conquerors.jpeg', name: 'Fabled Conquerors' , 'page': 5, 'id': 811 },
 { href: './html/Escaping the prison/index.html', imgSrc: 'escaping the prison.jpeg', name: 'Escaping the prison' , 'page': 5, 'id': 810 },
+{ href: './html/DOOM/index.html', imgSrc: 'doom.jpeg', name: 'DOOM' , 'page': 5, 'id': 795 },
 ]
